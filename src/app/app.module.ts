7d87b404f6f977f78e4dae5f6433dda632b44e7a
kuklin12 <mckuklina@gmail.com>
2019-07-23 12:16:15 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule} from '@angular/forms';
-import { RecipiesComponent } from './recipies/recipies.component';
-import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
-import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
-import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -15,10 +15,10 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent,
-    RecipieListComponent,
-    RecipieDetailComponent,
-    RecipieItemComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipie-list',
-  templateUrl: './recipie-list.component.html',
-  styleUrls: ['./recipie-list.component.css']
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipieListComponent implements OnInit {
+export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test Recipie', 'This is only a test','https://cdn.pixabay.com/photo/2016/01/14/09/47/food-1139580_960_720.jpg')
   ];
