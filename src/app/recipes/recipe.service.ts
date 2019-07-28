cfaa7ec5cca81c5e5622a029763e93d4fe809dd6
kpkmmk <mckuklina@gmail.com>
2019-07-28 18:03:00 +0200

    commit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -33,7 +33,9 @@ export class RecipeService {
   getRecipies() {
     return this.recipes.slice();
   }
-
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
